b479e878076f347d31691dcd29ef602df72c5321
minhdt102 <minhdt24082002@gmail.com>
2019-11-09 23:15:28 +0700

    session 3

--- Homework/session_3.py
+++ Homework/session_3.py
@@ -1,34 +1,43 @@
 def factorial(n = int):
-    if n < 0:
-        return "Can't calculate factorial of a negative number"        
-    elif n == 0 or n == 1:
-        return 1
+    if type(n) == int:
+        if n < 0:   
+            return "Input an integer larger than 0"        
+        elif n == 0 or n == 1:
+            return 1
+        else:
+            return n*factorial(n-1)
     else:
-        return n*factorial(n-1)
-
+        return "Input an integer"
 def fibonaci(n):
-    if n <= 0:
-        return "Input an integer larger than 0"
-    elif n == 1 :
-        return 0
-    elif n == 2:
-        return 1
+    if type(n) == int:
+        if n <= 0:
+            return "Input an integer larger than 0"
+        elif n == 1 :
+            return 0
+        elif n == 2:
+            return 1
+        else:
+            return fibonaci(n-1) + fibonaci(n-2)
     else:
-        return fibonaci(n-1) + fibonaci(n-2)
+        return "Input an integer"
         
 def pascal_triangle(n):
-    if n <= 0:
-        return "Input an integer larger than 0"
-    elif n == 1:
-        line = [1]
-    elif n == 2:
-        line = [1,1]
+    if type(n) == int:
+        if n <= 0:
+            return "Input an integer larger than 0"
+        elif n == 1:
+            line = [1]
+        elif n == 2:
+            line = [1,1]
+        else:
+            line = []
+            previous_line = pascal_triangle(n-1)
+            for i in range(len(previous_line) - 1):
+                line.append(previous_line[i] + previous_line[i+1])
+            line = [1] + line + [1]
+        return line
     else:
-        line = []
-        previous_line = pascal_triangle(n-1)
-        for i in range(len(previous_line) - 1):
-            line.append(previous_line[i] + previous_line[i+1])
-        line = [1] + line + [1]
-    return line
+        return "Input an integer"
 
-print(pascal_triangle(2))
+print(pascal_triangle(1.1))
+print(factorial(1.1))
