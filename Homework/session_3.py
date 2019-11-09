f5d32ca603dc8b1f4fec2446291ce07c41423f89
minhdt102 <minhdt24082002@gmail.com>
2019-11-09 23:07:32 +0700

    session 3

--- Homework/session_3.py
+++ Homework/session_3.py
@@ -20,15 +20,15 @@ def pascal_triangle(n):
     if n <= 0:
         return "Input an integer larger than 0"
     elif n == 1:
-        line = 1
+        line = [1]
     elif n == 2:
         line = [1,1]
     else:
         line = []
         previous_line = pascal_triangle(n-1)
         for i in range(len(previous_line) - 1):
-            
             line.append(previous_line[i] + previous_line[i+1])
         line = [1] + line + [1]
     return line
 
+print(pascal_triangle(2))
